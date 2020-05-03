eee1c7e8da6cb726c26eedc094f0e83f50385ac0
James Johnson <jtaylorjohnson@gmail.com>
2020-05-03 21:18:05 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -46,7 +46,7 @@ class Pokemon
   def self.new_from_db(row)
   id = row[0]
   name = row[1]
-  grade = row[2]
+  type = row[2]
   student = self.new(id, name, grade)
   student
   end
@@ -54,7 +54,7 @@ class Pokemon
   def self.find(name)
     sql = "SELECT * FROM pokemons WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    self.new_from_db(result)
+    self.new_from_db(result.flatten)
   end
   
   def update
